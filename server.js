55d47d4f8fb2647657523618bd2bd30e6eef2f0c
fudanzz <fudanzz@gmail.com>
2016-11-01 07:31:49 +0800

    update

--- server.js
+++ server.js
@@ -2,4 +2,26 @@
 // that will send back the index.html file on a GET request to '/'
 // it should then send back jsonData on a GET to /data
 
-var jsonData = {count: 12, message: 'hey'};
+var jsonData = {
+    count: 12,
+    message: 'hey'
+};
+
+var express = require('express');
+
+var app = express();
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html', function (err) {
+        if (err) {
+            res.status(500).send(err);
+        }
+    });
+})
+
+
+app.get('/data', function (req, res) {
+    res.json(jsonData);
+})
+
+app.listen(3000);
